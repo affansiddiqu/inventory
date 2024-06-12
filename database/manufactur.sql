c3c0cebe556851f1d7db7e96dbab4f74233b20a4
Affan <siddiquiaffan701@gmail.com>
2024-06-12 17:57:25 +0500

    ol

--- database/manufactur.sql
+++ database/manufactur.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2024 at 02:51 PM
+-- Generation Time: Jun 12, 2024 at 02:56 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -24,60 +24,97 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table `customer`
 --
 
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
-  `order_date` date NOT NULL,
-  `client_name` varchar(255) NOT NULL,
-  `client_contact` varchar(255) NOT NULL,
-  `sub_total` varchar(255) NOT NULL,
-  `vat` varchar(255) NOT NULL,
-  `total_amount` varchar(255) NOT NULL,
-  `discount` varchar(255) NOT NULL,
-  `grand_total` varchar(255) NOT NULL,
-  `paid` varchar(255) NOT NULL,
-  `due` varchar(255) NOT NULL,
-  `payment_type` int(11) NOT NULL,
-  `payment_status` int(11) NOT NULL,
-  `order_status` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE `customer` (
+  `Id` int(11) NOT NULL,
+  `Number` varchar(255) NOT NULL,
+  `Customer` varchar(255) NOT NULL,
+  `Datee` date NOT NULL,
+  `Reference` varchar(255) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Amount` int(11) NOT NULL,
+  `Status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `orders`
+-- Dumping data for table `customer`
 --
 
-INSERT INTO `orders` (`order_id`, `order_date`, `client_name`, `client_contact`, `sub_total`, `vat`, `total_amount`, `discount`, `grand_total`, `paid`, `due`, `payment_type`, `payment_status`, `order_status`) VALUES
-(1, '2023-05-08', 'Test', '123213', '20.00', '2.60', '22.60', '0', '22.60', '0', '22.60', 2, 3, 2),
-(2, '2023-05-10', 'AAA', '01', '1800.00', '234.00', '2034.00', '0', '2034.00', '1000', '1034.00', 2, 1, 2),
-(3, '2023-05-24', 'Audace', '07885541159', '12.00', '1.56', '13.56', '2', '11.56', '11.56', '-1.7763568394003E-15', 2, 1, 2);
+INSERT INTO `customer` (`Id`, `Number`, `Customer`, `Datee`, `Reference`, `Quantity`, `Amount`, `Status`) VALUES
+(7, 'SD-00001', 'naveed', '2024-06-12', 'qq\r\n\r\n', 0, 0, 1),
+(8, 'SD-00002', 'rohan', '2024-06-27', 'sa', 0, 0, 1),
+(9, 'SD-21474', 'Ramish', '2024-06-27', 'a', 0, 0, 1);
+
+--
+-- Triggers `customer`
+--
+DELIMITER $$
+CREATE TRIGGER `before_insert_Customer` BEFORE INSERT ON `customer` FOR EACH ROW BEGIN
+    DECLARE max_id INT;
+    DECLARE new_Number VARCHAR(10);
+
+    -- Find the maximum numeric part of the existing custom_ids
+    SELECT IFNULL(MAX(CAST(SUBSTRING(Number, 3) AS UNSIGNED)), 0) + 1 INTO max_id FROM Customer;
+
+    -- Generate the new custom_id with the prefix 'pi' and zero-padded number
+    SET new_Number = CONCAT('SD-', LPAD(max_id, 5, '0'));
+
+    -- Assign the new custom_id to the new row
+    SET NEW.Number = new_Number;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `befowre_insert_Customer` BEFORE INSERT ON `customer` FOR EACH ROW BEGIN
+    DECLARE max_id INT;
+    DECLARE new_Number VARCHAR(10);
+
+    -- Find the maximum numeric part of the existing custom_ids
+    SELECT IFNULL(MAX(CAST(SUBSTRING(Number, 3) AS UNSIGNED)), 0) + 1 INTO max_id FROM Customer;
+
+    -- Generate the new custom_id with the prefix 'pi' and zero-padded number
+    SET new_Number = CONCAT('SD-', LPAD(max_id, 5, '0'));
+
+    -- Assign the new custom_id to the new row
+    SET NEW.Number = new_Number;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order_item`
+-- Table structure for table `pro`
 --
 
-CREATE TABLE `order_item` (
-  `order_item_id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL DEFAULT 0,
-  `product_id` int(11) NOT NULL DEFAULT 0,
-  `quantity` varchar(255) NOT NULL,
-  `rate` varchar(255) NOT NULL,
-  `total` varchar(255) NOT NULL,
-  `order_item_status` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE `pro` (
+  `Id` int(11) NOT NULL,
+  `sid` int(11) NOT NULL,
+  `cod` varchar(10) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `order_item`
+-- Dumping data for table `pro`
 --
 
-INSERT INTO `order_item` (`order_item_id`, `order_id`, `product_id`, `quantity`, `rate`, `total`, `order_item_status`) VALUES
-(1, 1, 1, '1', '20', '20.00', 2),
-(6, 2, 1, '90', '10', '900.00', 2),
-(7, 2, 2, '90', '10', '900.00', 2),
-(8, 3, 9, '12', '1', '12.00', 2);
+INSERT INTO `pro` (`Id`, `sid`, `cod`, `Name`, `price`, `quantity`, `amount`, `status`) VALUES
+(15, 9, 'SD-00002', 'HS Navy Blue S', 23, 2, 46, 1),
+(17, 10, 'SD-00003', 'HS Yellow S', 100, 11, 1100, 1),
+(18, 10, 'SD-00003', 'HS Yellow S', 100, 11, 1100, 1),
+(19, 10, 'SD-00003', 'HS Yellow S', 100, 11, 1100, 1),
+(20, 10, 'SD-00003', 'HS Yellow S', 100, 11, 1100, 1),
+(21, 11, 'SD-00004', 'HS Navy Blue S', 23, 1, 23, 1),
+(22, 11, 'SD-00004', '266', 19, 121, 2299, 1),
+(23, 12, 'SD-00005', 'HS Black', 390, 100, 39000, 1),
+(24, 12, 'SD-00005', '278', 310, 50, 15500, 1),
+(25, 12, 'SD-00005', '266', 310, 50, 15500, 1);
 
 -- --------------------------------------------------------
 
@@ -102,9 +139,9 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`Id`, `Code`, `Name`, `Measurement`, `Category`, `Sales_Price`, `Purchase_Price`, `Status`) VALUES
 (266, 'P-00001', 'HS White', 'Pc', 'Polo', 19, 16, 1),
-(275, 'P-00002', 'HS Black', 'Pc', 'Tshirt', 390, 280, 1),
+(275, 'P-00002', 'HS Black', 'Pc', 'Tshirt', 390, 200, 1),
 (276, 'P-00003', 'HS Navy Blue S', 'Pc', 'Tshirt', 23, 12, 1),
-(277, 'P-00004', 'HS Yellow S', 'Pc', 'Tshirt', 310, 280, 1),
+(277, 'P-00004', 'HS Yellow S', 'Pc', 'Tshirt', 100, 80, 1),
 (278, 'P-00005', 'HS Pink S', 'Pc', 'Tshirt', 310, 280, 1),
 (279, 'P-00006', 'HS Green', 'Kg', 'Hoodies', 390, 330, 1);
 
@@ -172,7 +209,6 @@ CREATE TABLE `stock` (
 --
 
 INSERT INTO `stock` (`Id`, `P_id`, `SCode`, `Type`, `Date`, `Reference`, `Cost`, `Quantity`, `Amount`, `Status`) VALUES
-(233, 276, 'SA-00001', 'Opening Stock', '2024-07-04', 'HS Navy Blue S', 12, 2, 24, 1),
 (234, 278, 'SA-00002', 'Inward Stock', '2024-06-07', 'HS Navy Blue S', 12, 2, 24, 1),
 (235, 276, 'SA-00003', 'Inward Stock', '2024-06-11', 'HS Navy Blue S', 12, 21, 252, 1),
 (236, 277, 'SA-00004', 'Lost of Theft', '2024-05-31', 'HS Yellow S', 280, 1, 280, 1),
@@ -193,7 +229,11 @@ INSERT INTO `stock` (`Id`, `P_id`, `SCode`, `Type`, `Date`, `Reference`, `Cost`,
 (251, 266, 'SA-00019', 'Opening Stock', '2024-06-08', 'HS White', 11, 2, 22, 1),
 (252, 279, 'SA-00020', 'Opening Stock', '2024-05-29', 'HS Green', 330, 2, 660, 1),
 (253, 276, 'SA-00021', 'Opening Stock', '0002-02-01', 'HS Navy Blue S', 12, 1, 12, 1),
-(254, 276, 'SA-00022', 'Opening Stock', '0002-02-01', 'HS Navy Blue S', 12, 1, 12, 1);
+(254, 276, 'SA-00022', 'Opening Stock', '0002-02-01', 'HS Navy Blue S', 12, 1, 12, 1),
+(264, 278, 'SA-00023', 'Opening Stock', '2024-06-07', 'HS Pink S', 280, 1, 280, 1),
+(265, 275, 'SA-00024', 'Opening Stock', '2024-06-07', 'HS Black', 280, 2, 560, 1),
+(266, 266, 'SA-00025', 'Opening Stock', '2024-06-07', 'HS White', 16, 1, 16, 1),
+(267, 277, 'SA-00026', 'Opening Stock', '2024-06-07', 'HS Yellow S', 280, 1, 280, 1);
 
 --
 -- Triggers `stock`
@@ -223,13 +263,10 @@ DELIMITER ;
 
 CREATE TABLE `svaluation` (
   `Id` int(11) NOT NULL,
+  `Cid` int(11) NOT NULL,
   `Vcode` varchar(255) NOT NULL,
-  `P_id` int(11) NOT NULL,
-  `Customer` varchar(255) NOT NULL,
   `Date` date NOT NULL,
   `Vreference` varchar(255) NOT NULL,
-  `Pname` varchar(255) NOT NULL,
-  `Vcost` int(11) NOT NULL,
   `Vquantity` int(11) NOT NULL,
   `vamount` int(11) NOT NULL,
   `Address` text NOT NULL,
@@ -241,8 +278,12 @@ CREATE TABLE `svaluation` (
 -- Dumping data for table `svaluation`
 --
 
-INSERT INTO `svaluation` (`Id`, `Vcode`, `P_id`, `Customer`, `Date`, `Vreference`, `Pname`, `Vcost`, `Vquantity`, `vamount`, `Address`, `Comment`, `Status`) VALUES
-(55, 'SD-00008', 266, 'subhan', '2024-06-13', '2', 'HS White', 19, 2, 38, 'way on', 'best!', 1);
+INSERT INTO `svaluation` (`Id`, `Cid`, `Vcode`, `Date`, `Vreference`, `Vquantity`, `vamount`, `Address`, `Comment`, `Status`) VALUES
+(7, 7, 'SD-00001', '2024-05-30', 'aa', 2, 390, '', '', 1),
+(9, 7, 'SD-00002', '2024-05-30', 'gg', 2, 8626, '', '', 1),
+(10, 9, 'SD-00003', '2024-06-14', 'aa', 16, 1365, '', '', 1),
+(11, 7, 'SD-00004', '0001-01-01', '1', 122, 2322, '', '', 1),
+(12, 7, 'SD-00005', '2023-10-10', 'JO18', 200, 70000, '', '', 1);
 
 --
 -- Triggers `svaluation`
@@ -269,16 +310,17 @@ DELIMITER ;
 --
 
 --
--- Indexes for table `orders`
+-- Indexes for table `customer`
 --
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`);
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`Id`);
 
 --
--- Indexes for table `order_item`
+-- Indexes for table `pro`
 --
-ALTER TABLE `order_item`
-  ADD PRIMARY KEY (`order_item_id`);
+ALTER TABLE `pro`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `sid` (`sid`);
 
 --
 -- Indexes for table `products`
@@ -303,24 +345,24 @@ ALTER TABLE `stock`
 -- Indexes for table `svaluation`
 --
 ALTER TABLE `svaluation`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `P_id` (`P_id`);
+  ADD PRIMARY KEY (`Id`,`Vcode`),
+  ADD KEY `Cid` (`Cid`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT for table `customer`
 --
-ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `customer`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `order_item`
+-- AUTO_INCREMENT for table `pro`
 --
-ALTER TABLE `order_item`
-  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `pro`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -338,17 +380,23 @@ ALTER TABLE `signup`
 -- AUTO_INCREMENT for table `stock`
 --
 ALTER TABLE `stock`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=264;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=268;
 
 --
 -- AUTO_INCREMENT for table `svaluation`
 --
 ALTER TABLE `svaluation`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `pro`
+--
+ALTER TABLE `pro`
+  ADD CONSTRAINT `pro_ibfk_1` FOREIGN KEY (`sid`) REFERENCES `svaluation` (`Id`);
 
 --
 -- Constraints for table `stock`
@@ -360,7 +408,7 @@ ALTER TABLE `stock`
 -- Constraints for table `svaluation`
 --
 ALTER TABLE `svaluation`
-  ADD CONSTRAINT `svaluation_ibfk_1` FOREIGN KEY (`P_id`) REFERENCES `products` (`Id`);
+  ADD CONSTRAINT `svaluation_ibfk_1` FOREIGN KEY (`Cid`) REFERENCES `customer` (`Id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
